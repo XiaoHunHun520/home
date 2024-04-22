8c959083ed94f85bd7c1e1a9e224bde15a5a70c7
Penguin <129220411+XiaoHunHun520@users.noreply.github.com>
2024-04-22 23:05:47 +0800

    修改自己的配置文件

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, Search, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Search, Compass, LaptopCode, Hubspot } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -63,13 +63,10 @@ const siteLinksList = computed(() => {
 
 // 网站链接图标
 const siteIcon = {
-  Blog,
-  Cloud,
   Search,
   Compass,
-  Book,
-  Fire,
   LaptopCode,
+  Hubspot,
 };
 
 // 链接跳转
